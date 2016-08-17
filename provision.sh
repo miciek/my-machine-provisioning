476f0850d78c6cc139b7ee006c1ac7b890b22205
Michał Płachta <miciek@users.noreply.github.com>
2016-08-18 00:17:41 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo apt-get -qq -y install python-setuptools aptitude git python-dev libxml2-dev libxslt-dev gcc make build-essential libssl-dev libffi-dev
+sudo apt-get -y install python-setuptools aptitude git python-dev libxml2-dev libxslt-dev gcc make build-essential libssl-dev libffi-dev
 
 sudo easy_install pip
 
